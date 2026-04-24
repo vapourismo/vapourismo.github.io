8586f94f7ddca8fb2ed67d22fffa31415d78d1af
Ole Krüger <git@ole.lol>
2026-04-24 13:38:11 +0100

    Bootstrap basic Nix Flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nil
+            nixd
+            nixfmt
+          ];
+        };
+
+        formatter = pkgs.nixfmt-tree;
+      }
+    );
+}
